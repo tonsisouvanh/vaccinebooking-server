ecb140eefb0a11a17d880cb03c81094097b93672
Tons <38555090+tonsisouvanh@users.noreply.github.com>
2023-03-08 22:24:29 +0700

    fourth attemp deploy

--- src/index.js
+++ src/index.js
@@ -14,9 +14,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send('Vaccine server is running...');
-});
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/vaccines", require("./routes/vaccineRoutes"));
 app.use("/api/admin", require("./routes/adminRoutes"));
